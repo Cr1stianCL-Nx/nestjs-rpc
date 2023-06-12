e52f4305feb35cc36041159fa0ed564722915204
Cristian Rosas <cristian.rosas@protonmail.com>
2023-06-11 23:07:19 -0400

    added initial push with base

---
+++ apps/nestsjs-rpc/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'nestsjs-rpc',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/nestsjs-rpc'
+};
